10e9858673ac7487e0810711c26b6e2abb1c3e01
mansur/galimov <mansurgalimov161@gmail.com>
2022-09-12 15:04:33 +0300

    Added directory with Python tasks  due 25.09.22, in OAIP

---
+++ pyprojects/task1.3.py
@@ -0,0 +1,16 @@
+a = float(input("Enter a: ")) # Input a, 
+b = float(input("Enter b: ")) # b,
+c = float(input("Enter c: ")) # and c
+
+if (a < b and b < c) or (a>b and b>c): # checking for condition which pointed out in brackets
+    a = a * 2 # multi a,
+    b = b * 2 # b,
+    c = c * 2 # and c
+    print("A: ", a," - B: ",b, " - C: ", c) # putting in stdout
+    exit()
+else: # if condition has not been fitted
+    a = a * (-1) # turn into nagative values a,
+    b = b * (-1) # b,
+    c = c * (-1) # and c
+    print("Changing sign: ", "A: ", a," - B: ", b , "- C: ", c) # putting instdout
+    exit()
---
+++ pyprojects/task1.py
@@ -0,0 +1,8 @@
+a = int(input("Enter number : ")) # get number from stdin
+if a > 0: # checking for positive value
+    a = a + 1 # if positive, adding 1 
+    print("Number :", a, " = positive") # putting number to stdout
+else: # if number is less or equal to 0
+    print("Number is less or equal to 0, so hasn't been changed") # message about action abortion
+    print("Num : ", a) # printing a 
+
