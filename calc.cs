8c2edb1f1b884266724938996ae84a0462e88ba6
mansur/galimov <mansurgalimov161@gmail.com>
2022-09-26 11:42:01 +0300

    Minor fixes

--- calc.cs
+++ calc.cs
@@ -16,14 +16,11 @@ namespace Calc
 		try
 		{
 			Console.Write("Enter a first number: ");
-			string a_msg = Console.ReadLine();
-			double a = Convert.ToDouble(a_msg);
+			double a = Convert.ToDouble(Console.ReadLine());
 			Console.Write("Enter a second number: ");
-			string b_msg = Console.ReadLine();
-			double b = Convert.ToDouble(b_msg);
+			double b = Convert.ToDouble(Console.ReadLine());
 			Console.Write("Enter a number of action: ");
-			string acttoi = Console.ReadLine();
-			int act_chosen = Convert.ToInt32(acttoi);
+			int act_chosen = Convert.ToInt32(Console.ReadLine());
 			if (act_chosen == 1) 
 			{
 				c = a + b;
@@ -41,18 +38,44 @@ namespace Calc
 					Console.WriteLine("result (div) " +c);
 			} else if (act_chosen == 5)
 			{
-					c = Math.Pow(a,b);
-					Console.WriteLine("result (pow) " +c);
+				double res = 1;
+				 Console.Write("Enter a power to increase the number: ");
+       				int orig_pow = Convert.ToInt32(Console.ReadLine());
+				for (int i = 1; i <= orig_pow; i++)		
+					{
+						res = res*a;
+
+					}
+				Console.WriteLine("result (pow) on a: "+res);
+				Console.Write("Do you want perform such action with B? (y/n): ");
+				string asrt2 = Console.ReadLine();
+				if (asrt2 == "y" || asrt2 == "yes" || asrt2 == "Yes")
+				{
+					double res2 = 1;
+					Console.Write("Enter a power to increase the number: ");
+					int orig_pow2  = Convert.ToInt32(Console.ReadLine());
+					for (int j = 1; j<= orig_pow2; j++)
+					{
+						res2 = res2 * b;	
+					}
+					Console.WriteLine("result (pow) on b: "+res2);
+				} else if (asrt2 == "n" ||asrt2 == "no" || asrt2 == "No" )
+				{
+					continue;
+				} else
+				{
+					continue;
+				}
 			} else if (act_chosen == 6)
 			{
 					c = Math.Sqrt(a);
-					Console.WriteLine("result (sqrt) " +c);
-					Console.Write("Do you want perform sqrt action with B ? (y/n): ");
+					Console.WriteLine("result (sqrt): " +c);
+					Console.Write("Do you want perform such action with B? (y/n): ");
 					string asrt = Console.ReadLine();
 					if (asrt == "y" || asrt == "yes" || asrt == "Yes")
 					{
 						double c1 = Math.Sqrt(b);
-						Console.WriteLine("result (sqrt) with b " +c1);
+						Console.WriteLine("result (sqrt) with b: " +c1);
 					} else if (asrt == "n" || asrt == "no" || asrt == "No")
 					{
 						continue;
@@ -63,15 +86,15 @@ namespace Calc
 					a = a / 100;
 					a = a * 1;
 					c = a;
-					Console.WriteLine("result (percent) from a % = " +c);
-					Console.Write("Do you want perform 1 percent action with B ? (y/n): ");
+					Console.WriteLine("result (percent) from a % : " +c);
+					Console.Write("Do you want perform such action with B? (y/n): ");
 					string asrt1 = Console.ReadLine();
 					if (asrt1 == "y" || asrt1 == "yes" || asrt1 == "Yes")
 					{      
 					       	b = b / 100;
 						b = b * 1;
 						double c2 = b;
-						Console.WriteLine("result (percent) from  b % = " +c2);
+						Console.WriteLine("result (percent) from  b % : " +c2);
 					} else if (asrt1 == "n" || asrt1 == "no" || asrt1 == "No") 
 					{
 						continue;
@@ -79,12 +102,30 @@ namespace Calc
 						continue;
 			} else if (act_chosen == 8)
 			{
-					a = a * (b - 1);
-					c = a;
-					Console.WriteLine("result (fact) " +c);
+				double fact = 1;
+				for (int h = 1; h <= a; h++)
+				{
+					fact = fact * h;
+				}
+					Console.WriteLine("result (fact): " +fact);
+					Console.Write("Do you want perform such action with B? (y/n): ");
+					string asrt3 = Console.ReadLine();
+					if (asrt3 == "y" || asrt3 == "yes" || asrt3 == "Yes")
+					{
+						double fact2 = 1;
+						for (int y = 1; y <= b; y++)
+						{
+							fact2 = fact2 * y;
+						}
+						Console.WriteLine("result (fact) for b: "+fact2);
+					} else if (asrt3 == "n" ||  asrt3 == "no" || asrt3 == "No")
+					{
+						continue;
+					} else
+						continue;
 			} else if (act_chosen == 9)
 			{
-				Console.Write("Exiting...");
+				Console.WriteLine("Exiting...");
 				break;
 			} else if (act_chosen == 10)
 			{
